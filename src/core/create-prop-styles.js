f9aea326e0606576475f5a67aae19b04a86e53dd
John Grishin <r@exah.ru>
2018-05-22 10:58:54 +0300

    ♻️ Filter empty styles in prop styles

--- src/core/create-prop-styles.js
+++ src/core/create-prop-styles.js
@@ -2,24 +2,27 @@ import { keys, toPairs, once } from 'ramda'
 import { wrapIfMedia, getStyles, themeMedia } from '../utils'
 
 const propStyles = (stylesMap) => (props) =>
-  toPairs(props).map(([ key, val ]) =>
-    getStyles(stylesMap[key], val, props)
-  )
+  toPairs(props)
+    .map(([ key, val ]) => [ stylesMap[key], val, props ])
+    .filter(([ style ]) => style != null)
+    .map((args) => getStyles(...args))
 
 const buildMediaRegEx = once((media) =>
   new RegExp('(' + keys(media).join('|') + ')?$')
 )
 
 const mediaPropStyles = (stylesMap, label) => (props) => {
-  const media = themeMedia(props)
+  const { theme, ...rest } = props
+  const media = themeMedia(theme)
   const mediaRegEx = buildMediaRegEx(media)
-  const result = toPairs(props)
+  const result = toPairs(rest)
     .map(([ key, val ]) => {
       if (stylesMap[key]) return [ stylesMap[key], val ]
 
       const [ styleKey, mediaKey ] = key.split(mediaRegEx)
       return [ stylesMap[styleKey], val, mediaKey ]
     })
+    .filter(([ style ]) => style != null)
     .map(([ style, val, mediaKey ]) => wrapIfMedia(
       media[mediaKey],
       getStyles(style, val, props, mediaKey)
