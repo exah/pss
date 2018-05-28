7651917268d5dcf388c982d8c2d4e2e3b68519a5
John Grishin <r@exah.me>
2018-05-28 16:31:32 +0300

    ✨ Support customCssValue in colorProp

--- src/core/theme-prop.js
+++ src/core/theme-prop.js
@@ -1,4 +1,4 @@
-import { curryN } from 'ramda'
+import { curryN, identity } from 'ramda'
 import { CSS_DEFAULT_VALUE, CSS_PROPS_DEFAULTS } from '../constants'
 import { getColors, getColor, isColor } from '../utils'
 
@@ -19,17 +19,19 @@ const themeProp = (bgKey, fgKey) => curryN(2, (value, { theme }) => {
   }
 })
 
-const colorProp = (cssProp, colorKey) => curryN(2, (value, { theme }) => {
-  if (value == null) return {}
+const colorProp = (cssProp, colorKey, getCssValue = identity) => curryN(2,
+  (value, props) => {
+    if (value == null) return {}
 
-  const color = value === false
-    ? CSS_PROPS_DEFAULTS[cssProp] || CSS_DEFAULT_VALUE
-    : isColor(value) ? value : getColor(theme, colorKey, value)
+    const color = value === false
+      ? CSS_PROPS_DEFAULTS[cssProp] || CSS_DEFAULT_VALUE
+      : isColor(value) ? value : getColor(props.theme, colorKey, value)
 
-  return !color ? {} : {
-    [cssProp]: color
+    return !color ? {} : {
+      [cssProp]: getCssValue(color, props)
+    }
   }
-})
+)
 
 export {
   colorProp,
