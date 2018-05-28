4b54d2269b54cbceb88861af9ae9c75b51de9143
John Grishin <r@exah.ru>
2018-05-28 11:03:38 +0300

    🔥 Remove unused isPlainObj function

--- src/utils/is.js
+++ src/utils/is.js
@@ -6,9 +6,6 @@ const isNum = is(Number)
 const isStr = is(String)
 const isArr = is(Array)
 const isObj = is(Object)
-const isPlainObj = (val) => (
-  isObj(val) && Object.getPrototypeOf(Object.getPrototypeOf(val)) == null
-)
 
 export {
   isFn,
@@ -16,6 +13,5 @@ export {
   isNum,
   isStr,
   isArr,
-  isObj,
-  isPlainObj
+  isObj
 }
