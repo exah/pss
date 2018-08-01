a6a39aa8984572c2e91cfdd2aac9cd7eddcff7d7
John Grishin <r@exah.ru>
2018-08-01 10:34:41 +0300

    🚑 Fix `theme` style keys

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -2,9 +2,9 @@ import { themeProp, colorProp } from '../core'
 
 const defaultPalette = (style) => (val = true) => style(val)
 
-const theme = defaultPalette(themeProp('background', 'foreground'))
-const color = defaultPalette(colorProp('color', 'foreground'))
-const backgroundColor = defaultPalette(colorProp('backgroundColor', 'background'))
+const theme = defaultPalette(themeProp('bg', 'fg'))
+const color = defaultPalette(colorProp('color', 'fg'))
+const backgroundColor = defaultPalette(colorProp('backgroundColor', 'bg'))
 const borderColor = defaultPalette(colorProp('borderColor', 'border'))
 
 export { theme, color, backgroundColor, borderColor }
