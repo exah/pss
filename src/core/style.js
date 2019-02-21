664177dc782292aec888cd06139343243a6f577e
Ivan Grishin <r@exah.me>
2019-02-21 19:11:18 +0300

    ✨ Add `style`

---
+++ src/core/style.js
@@ -0,0 +1,11 @@
+import { createStyles } from './create-styles'
+import { createRule } from './rule'
+
+export const style = ({
+  prop,
+  getValue,
+  cssProp = prop,
+  rule = createRule({ cssProp, getValue })
+}) => createStyles({
+  [prop]: rule
+})
