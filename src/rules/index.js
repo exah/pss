81fa5ed6442698a6a5b052a1cb222fcc37b482ba
Ivan Grishin <r@exah.me>
2019-02-21 19:18:52 +0300

    🎨 Format rules

--- src/rules/index.js
+++ src/rules/index.js
@@ -1,6 +1,11 @@
 import { rule } from '../core'
 import { spaceValue, sizeValue, boolValue, percentageValue } from '../values'
 
-export const spaceRule = (name) => rule(name, spaceValue(sizeValue()))
-export const directionRule = (cssProp) => rule(cssProp, sizeValue(percentageValue(boolValue(0, 'auto'))))
-export const sizeRule = (cssProp, trueVal = '100%', falseVal = 0) => rule(cssProp, sizeValue(percentageValue(boolValue(trueVal, falseVal))))
+export const spaceRule = (name) =>
+  rule(name, spaceValue(sizeValue()))
+
+export const directionRule = (cssProp) =>
+  rule(cssProp, sizeValue(percentageValue(boolValue(0, 'auto'))))
+
+export const sizeRule = (cssProp, trueVal = '100%', falseVal = 0) =>
+  rule(cssProp, sizeValue(percentageValue(boolValue(trueVal, falseVal))))
