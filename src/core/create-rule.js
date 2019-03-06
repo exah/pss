c65e94734e6dcf0ca8c85888015f7ff64927d3ff
John Grishin <r@exah.ru>
2019-03-07 01:06:41 +0300

    ♻️ Move `everyMedia` inside `createRule`

--- src/core/create-rule.js
+++ src/core/create-rule.js
@@ -1,6 +1,18 @@
-import { isBool, isNum, isFn, identity, isStr } from '@exah/utils'
-import { everyMedia } from '../core/every-media'
-import { wrap } from '../utils'
+import { isBool, isNum, isFn, identity, isStr, mapObj, isObj } from '@exah/utils'
+import { wrap, wrapIfMedia } from '../utils'
+import { getThemeMedia } from '../getters'
+
+const has = (a, b) => b.some((key) => a.includes(key))
+
+function everyMedia (props, value, wrapper = identity) {
+  const media = getThemeMedia(props)
+
+  if (isObj(value) && has(Object.keys(media), Object.keys(value))) {
+    return mapObj((k, v) => wrapIfMedia(media[k], wrapper(v)), value)
+  }
+
+  return wrapper(value)
+}
 
 /**
  * ```js
--- src/core/index.js
+++ src/core/index.js
@@ -1,7 +1,6 @@
 export * from './combine-styles'
 export * from './create-rule'
 export * from './create-styles'
-export * from './every-media'
 export * from './media-style'
 export * from './rule'
 export * from './style'
